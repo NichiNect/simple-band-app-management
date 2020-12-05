64e81b791b131c228f1227a635f84604985146df
NichiNect <yoniwidhi2@gmail.com>
2020-12-05 17:55:48 +0700

    genre seeder

---
+++ database/seeders/GenreSeeder.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Genre;
+use Illuminate\Database\Seeder;
+
+class GenreSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $genres = collect([
+            'Heavy Metal',
+            'Speed Metal',
+            'Thrash Metal',
+            'Power Metal',
+            'Death Metal',
+            'Black Metall',
+            'Pagan Metal',
+            'Viking Metal',
+            'Folk Metal',
+            'Symphonic Metal',
+            'Gothic Metal',
+            'Glam Metal',
+            'Hair Metal',
+            'Doom Metal',
+            'Groove Metal',
+            'Industrial Metal',
+            'Modern Metal',
+            'Neoclassical Metal',
+            'New Wave Of British Heavy Metal',
+            'Post Metal',
+            'Progressive Metal',
+            'Avantgarde Metal',
+            'Sludge',
+            'Djent',
+            'Drone',
+            'Kawaii Metal',
+            'Pirate Metal',
+            'Nu Metal',
+            'Neue Deutsche Härte',
+            'Math Metal',
+        ]);
+
+        $genres->each(function($genre) {
+            Genre::create([
+                'name' => $genre,
+                'slug' => \Str::slug($genre),
+            ]);
+        });
+    }
+}
